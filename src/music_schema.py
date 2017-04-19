8ee55800a57666f9e82efba4da781ec92814e305
I852318 <elizabeth.brooks@sap.com>
2017-04-19 13:56:18 -0700

    added monogoengine schema

---
+++ src/music_schema.py
@@ -0,0 +1,54 @@
+from mongoengine import *
+
+connect('track_scraper')
+
+
+class Artist(Document):
+    name = StringField(required=True)
+    aliases = ListField(StringField())
+    description = StringField()
+    homepage = StringField()
+
+
+class Collection(Document):
+    name = StringField(required=True)
+    artists = ListField(ReferenceField(Artist))
+    description = StringField()
+    picture_link = StringField()
+    tracks = ListField(ReferenceField(Track))
+    genres = ListField(ReferenceField(Genre))
+    associations = ListField(ReferenceField(Association))
+
+    meta = {'allow_inheritance': True}
+
+
+class Album(Collection):
+    release_date = StringField(max_length=50)
+
+
+class Track(Document):
+    name = StringField(required=True)
+    artists = ListField(ReferenceField(Artist))
+    description = StringField()
+    play_time = StringField()
+    lyrics = StringField()
+    language = StringField()
+    youtube_link = StringField()
+    genres = ListField(ReferenceField(Genre))
+    associations = ListField(ReferenceField(Association))
+
+    meta = {'allow_inheritance': True}
+
+
+class Remix(Track):
+    original_tracks = ListField(ReferenceField(Track))
+
+
+class Genre(Document):
+    name = StringField(required=True)
+    description = StringField()
+
+
+class Association(Document):
+    name = StringField(required=True)
+    description = StringField()
