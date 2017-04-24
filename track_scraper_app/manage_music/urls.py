1e5fd4163cfb5ec82cbcfc0a38093bc65aa4b493
I852318 <elizabeth.brooks@sap.com>
2017-04-24 15:09:23 -0700

    added template functionality

--- track_scraper_app/manage_music/urls.py
+++ track_scraper_app/manage_music/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from manage_music import views
+from . import views
 
+app_name = 'manage_music'
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^about/$', views.AboutPageView.as_view()),
+    url(r'^$', views.HomePageView.as_view(), name='index'),
+    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
 ]
--- track_scraper_app/manage_music/views.py
+++ track_scraper_app/manage_music/views.py
@@ -4,13 +4,20 @@ from django.views.generic import TemplateView
 #
 # user = authenticate(username=username, password=password)
 # assert isinstance(user, mongoengine.django.auth.User)
+from .models import Track
 
 
-# Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        top_alphabetical_track_list = Track.objects.order_by('-name')[:5]
+        context = {'top_alphabetical_track_list': top_alphabetical_track_list}
+        return render(request, 'manage_music/index.html', context)
 
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+
+
+# class MusicDatabaseView(TemplateView):
+#     def get(self, request, **kwargs):
+#
