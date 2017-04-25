d2dbb3f055c5dc5e4b22669c170827a38ab34829
I852318 <elizabeth.brooks@sap.com>
2017-04-25 12:00:49 -0700

    added favicon

--- track_scraper_app/track_scraper_app/urls.py
+++ track_scraper_app/track_scraper_app/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('manage_music.urls')),
+    url(
+        r'^favicon.ico$',
+        RedirectView.as_view(
+            url="/static/favicon.ico",
+            permanent=False),
+        name="favicon"
+    ),
 ]
