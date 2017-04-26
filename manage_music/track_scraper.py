6a0b657fe74bb9b20a6ef97976bd4d43ede2e8c8
I852318 <elizabeth.brooks@sap.com>
2017-04-26 10:22:41 -0700

    reformatted track_scraper to use mongodb

--- manage_music/track_scraper.py
+++ manage_music/track_scraper.py
@@ -1,48 +1,79 @@
-# import sys
-
-from .track_scraper_utils import get_webpage_soup
+from requests import get
 import logging
+from datetime import datetime
+
+from manage_music.models import Track, Artist, Recording, Album
+
 
 log = logging.getLogger(__name__)
 
 
-def scraper_practice(possible_proxy_settings):
+def scraper_practice():
     log.info("Starting Scraper Practice")
-    # TODO: figure out when to stop scraping infinite scroll pages
-    i = 0
-    while i < 77:
-        webpage_url = "https://8tracks.com/users/dakre/favorite_tracks" + "?page=" + str(i)
-        webpage_soup = get_webpage_soup(webpage_url, possible_proxy_settings)
-        all_fav_tracks = webpage_soup.find_all("li", class_="track fav_track")
-        scrape_section(all_fav_tracks)
-        i += 1
+    request_json = get_request_json("dakre", 1)
+    favorite_tracks_pagination = request_json["pagination"]
+    total_pages = favorite_tracks_pagination["total_pages"]
+    for index in range(1, total_pages):
+        request_json = get_request_json("dakre", index)
+        all_favorite_tracks = request_json["favorite_tracks"]
+        for favourite_track in all_favorite_tracks:
+            # TODO: there's a way to grab the artist info when playing tracks
+            # todo: investigate for aliases, description, homepage
+            artist = save_artist(favourite_track)
+            track = save_track(artist, favourite_track)
+            save_recording(favourite_track, track)
+            save_album(artist, favourite_track, track)
+            # "name": "Toy",
+            # "performer": "에프엑스(f(x))",
+            # "year": 20130729,
+            # 2013 07 29
+            # "release_name": "Pink Tape (Vol. 2)",
     log.info("Finished Scraper Practice")
 
 
-def scrape_section(all_fav_tracks):
-    for fav_track in all_fav_tracks:
-        fav_track_info = fav_track.find(class_="track_info")
-        fav_track_title = fav_track_info.find(class_="t").string
-        fav_track_artist = fav_track_info.find(class_="a").string
-        fav_track_details = fav_track.find(class_="track_details")
-        fav_track_album = ""
-        if fav_track_details.find(class_="album"):
-            if fav_track_details.find(class_="album").find(class_="detail").contents:
-                fav_track_album = fav_track_details.find(class_="album").find(class_="detail").string
-        fav_track_year = ""
-        if fav_track.find(class_="year"):
-            fav_track_year = fav_track.find(class_="year").find(class_="detail").string
-        log.info(fav_track_title + " by " + fav_track_artist + " in " + fav_track_album + ", " + fav_track_year)
-#
-#
-# # Main Function Call
-# if __name__ == '__main__':
-#     # TODO: Make manual page for proxy settings input (and user input)
-#     proxy_settings = {}
-#     if sys.argv[1]:
-#         http_proxy = str(sys.argv[1])
-#         proxy_settings.update({"http": http_proxy})
-#     if sys.argv[2]:
-#         https_proxy = str(sys.argv[2])
-#         proxy_settings.update({"https": https_proxy})
-#     scraper_practice(proxy_settings)
+def get_request_json(user, index):
+    webpage_url = "https://8tracks.com/users/" + str(user) + "/favorite_tracks" + \
+                  "?page=" + str(index) + "&format=jsonh"
+    request = get(webpage_url)
+    request_json = request.json()
+    return request_json
+
+
+def save_artist(favourite_track):
+    artist = Artist()
+    artist.name = favourite_track["performer"]
+    artist.save()
+    return artist
+
+
+def save_track(artist, favourite_track):
+    track = Track()
+    track.name = favourite_track["name"]
+    track.artists = [artist]
+    track.save()
+    return track
+
+
+def save_recording(favourite_track, track):
+    recording = Recording()
+    recording.tracks = [track]
+    recording.web_page = favourite_track["url"]
+    recording.save()
+
+
+def save_album(artist, favourite_track, track):
+    album = Album()
+    album.name = favourite_track["release_name"]
+    album.artists = [artist]
+    album.tracks = [track]
+    album.release_date = get_release_date(favourite_track["year"])
+    album.save()
+
+
+def get_release_date(integer_date):
+    if len(str(integer_date)) == 4:
+        return datetime.strptime(str(integer_date), "%Y")
+    if len(str(integer_date)) == 6:
+        return datetime.strptime(str(integer_date), "%Y%m")
+    if len(str(integer_date)) == 8:
+        return datetime.strptime(str(integer_date), "%Y%m%d")
--- manage_music/track_scraper_utils.py
+++ manage_music/track_scraper_utils.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 import os
 
 
-# Settings
-os.environ['NO_PROXY'] = 'localhost'
-
-
-def get_webpage_soup(webpage_url, proxy_settings):
-    if proxy_settings[0] and proxy_settings[1]:
-        webpage = requests.get(webpage_url, proxies=proxy_settings)
-    else:
-        webpage = requests.get(webpage_url)
-    webpage_soup = BeautifulSoup(webpage.text, "html.parser")
-    return webpage_soup
+# # Settings
+# os.environ['NO_PROXY'] = 'localhost'
+#
+#
+# def get_webpage_soup(webpage_url, proxy_settings):
+#     if proxy_settings[0] and proxy_settings[1]:
+#         webpage = requests.get(webpage_url, proxies=proxy_settings)
+#     else:
+#         webpage = requests.get(webpage_url)
+#     webpage_soup = BeautifulSoup(webpage.text, "html.parser")
+#     return webpage_soup
